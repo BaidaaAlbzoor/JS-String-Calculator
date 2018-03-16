fd0b5494b75e575b5d6db62057a3bd196879579b
baidaa albzoor <baidaaalbzoor@outlook.com>
2018-03-17 00:04:39 +0200

    Refactor "add" function to support different delimiters

--- calculator.js
+++ calculator.js
@@ -2,20 +2,23 @@
 function add(stringNumbers) {
 
     let sum = 0;
-    let numbers = stringNumbers.split(/[,\n]/);
+    var delimeter = /[,\n]/;
+    var numbers = stringNumbers.split(delimeter);
+
+
+    if (stringNumbers.substring(0, 2) === '//') {
+        delimeter = stringNumbers.substring(2, stringNumbers.indexOf('\n'));
+        let nums = stringNumbers.substring(stringNumbers.indexOf('\n') + 1);
+        numbers = nums.split(delimeter);
+    }
+
 
     numbers.forEach(item => {
-        let number = parseInt(item);
+        var number = parseInt(item);
 
-        if (isNaN(number) || number > 1000) {
-            number = 0;
-        }
+        if (isNaN(number) || number > 1000) { number = 0; }
 
-
-        if (number < 0) {
-            throw 'negative numbers not allowed';
-        }
-
+        if (number < 0) { throw 'negative numbers not allowed'; }
 
         sum += number;
     });
