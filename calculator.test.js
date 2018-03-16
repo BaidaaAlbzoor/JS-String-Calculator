6075eaf9d7102f7a163ead68c14b75ee4918e933
baidaa albzoor <baidaaalbzoor@outlook.com>
2018-03-16 22:17:43 +0200

    Add test for one value input

--- calculator.test.js
+++ calculator.test.js
@@ -4,3 +4,9 @@ const add = require('./calculator.js');
 test('return 0 if input is ""', () => {
     expect(add("")).toBe(0);
   });
+
+  
+  test('return 1 if input is "1"', () => {
+    expect(add("1")).toBe(1);
+  });
+  
